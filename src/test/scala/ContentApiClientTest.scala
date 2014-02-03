b63e25abe53aacf4460e1a481fd18426deffb35f
Francis Carr <fran@fcarr.com>
2014-02-03 23:22:17 +0000

    Add test for getting uk front editorsPicks

--- src/test/scala/ContentApiClientTest.scala
+++ src/test/scala/ContentApiClientTest.scala
@@ -1,6 +1,6 @@
 package test
 
-import contentapiclient.ContentApiClient
+import contentapiclient.{ContentApiQuery, ContentApiClient}
 import org.scalatest.{ShouldMatchers, FlatSpec}
 import org.scalatest.concurrent.{ScalaFutures, Futures}
 import org.scalatest.time.{Millis, Seconds, Span}
@@ -17,12 +17,24 @@ class ContentApiClientTest extends FlatSpec with ShouldMatchers with Futures wit
     }
   }
 
-  it should "get Content type" in {
+  it should "get Content" in {
     val contentResult = ContentApiClient.getContent("artanddesign/2013/nov/28/janet-delaney-best-photograph")
     whenReady(contentResult) { content =>
       content.isDefined should be (true)
       content.get.status should be ("ok")
       content.get.content.get.id should be ("artanddesign/2013/nov/28/janet-delaney-best-photograph")
+    }
+  }
+
+  it should "get Results" in {
+    val queryParams = ContentApiQuery("uk")
+      .withEdition("uk")
+      .withShowEditorsPicks(true)
+    val contentResult = ContentApiClient.getContent(queryParams)
+    whenReady(contentResult) { response =>
+      response.isDefined should be (true)
+      response.get.status should be ("ok")
+      response.get.editorsPicks.get.length should be > 15
     }
   }
 
