7eed6b6be7e15ea105df1c0e2110ea727e6f040d
Francis Carr <fran@fcarr.com>
2014-02-20 00:10:34 +0000

    Add a patience config for ContentApiQueryTest

--- src/test/scala/ContentApiQueryTest.scala
+++ src/test/scala/ContentApiQueryTest.scala
@@ -3,8 +3,12 @@ package test
 import org.scalatest.{ShouldMatchers, FlatSpec}
 import contentapiclient.ContentApiQuery
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
 
 class ContentApiQueryTest extends FlatSpec with ShouldMatchers with ScalaFutures {
+
+  implicit val defaultPatience =
+    PatienceConfig(timeout = Span(2, Seconds), interval = Span(5, Millis))
 
   "ContentApiQuery" should "query for an id properly through id" in {
     whenReady(ContentApiQuery("uk/culture").get) { response =>
